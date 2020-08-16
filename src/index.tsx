ce0498ad70ea940b44a9d280e2b9a79a331f8d4d
Nick Wylynko <nick1014375@gmail.com>
2020-08-16 20:52:42 +0800

    encode name correctly

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,12 @@ function Badge(
     React.ImgHTMLAttributes<HTMLImageElement> & { name: string }
 ) {
   const hex = badges[props.name]
+  const encodedName = props.name
+    .replace(/-/g, '--')
+    .replace(/_/g, '__')
+    .replace(/ /g, '_')
   const src = encodeURI(
-    `https://img.shields.io/badge/${props.name} -#${hex}.svg?&style=for-the-badge&logo=${props.name}&logoColor=white`
+    `https://img.shields.io/badge/${encodedName} -#${hex}.svg?&style=for-the-badge&logo=${props.name}&logoColor=white`
   ).replace('#', '%23')
   return <img src={src} alt={props.name} {...props} />
 }
