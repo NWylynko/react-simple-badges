3bd7eba507cdad425ac5b741c11c057d26983846
Nick Wylynko <nick1014375@gmail.com>
2020-08-16 20:24:36 +0800

    fix tests and json parse

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Badge from './index'
+import badges from '../fetch/data.min.json'
 
-Object.keys(Badge).map((item) => {
-  it(`renders ${item} without crashing`, () => {
+badges.forEach((item: { name: string; hex: string }) => {
+  it(`renders ${item.name} without crashing`, () => {
     const div = document.createElement('div')
-    const TestElement = Badge[item]
-    ReactDOM.render(<TestElement />, div)
+    ReactDOM.render(<Badge name={item.name} />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 })
