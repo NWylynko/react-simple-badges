8816a7cf5297b665cdec8cde378a4ba75cee9c40
Nick Wylynko <nick1014375@gmail.com>
2020-08-18 00:03:33 +0800

    add label a logoColor props

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,9 +5,9 @@ import Badge from 'react-simple-badges'
 const App = () => {
   return (
     <>
-      <Badge name="node.js" style={{width: 200}} />
-      <Badge name="YouTube Gaming" />
-      <Badge name="Valve" />
+      <Badge name="node.js" style={{width: 200}} logoColor="#eb4034" />
+      <Badge name="YouTube Gaming" label="Youtube" />
+      <Badge name="Valve" label="Steam" style={{padding: 10}} />
       <Badge name="Tesla" />
     </>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,31 @@ import badges from './data.json'
 function Badge(
   props: JSX.IntrinsicAttributes &
     React.ClassAttributes<HTMLImageElement> &
-    React.ImgHTMLAttributes<HTMLImageElement> & { name: string }
+    React.ImgHTMLAttributes<HTMLImageElement> & {
+      name: string
+      logoColor?: string
+      label?: string
+    }
 ) {
-  const hex = badges[props.name]
-  const encodedName = props.name
-    .replace(/-/g, '--')
-    .replace(/_/g, '__')
-    .replace(/ /g, '_')
+  const { name, logoColor, label } = props
+  const hex = '#' + badges[name]
+
+  if (logoColor && !isHex(logoColor)) {
+    throw new Error('color string isnt hex color')
+  }
+
+  const encodedName =
+    label || name.replace(/-/g, '--').replace(/_/g, '__').replace(/ /g, '_')
+
   const src = encodeURI(
-    `https://img.shields.io/badge/${encodedName} -#${hex}.svg?&style=for-the-badge&logo=${props.name}&logoColor=white`
-  ).replace('#', '%23')
+    `https://img.shields.io/badge/${encodedName} -${hex}.svg?&style=for-the-badge&logo=${name}&logoColor=${
+      logoColor || 'white'
+    }`
+  ).replace(/#/g, '%23')
+
   return <img src={src} alt={props.name} {...props} />
 }
+
+const isHex = (color: string): boolean => /^#[0-9A-Fa-f]{6}$/i.test(color)
 
 export default Badge
