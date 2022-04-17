772f4c304cf6e55a81aac5218d3166d3ce622994
Auto Fetch <nick@wylynko.com>
2022-04-17 01:01:27 +0000

    Updated json

--- src/badges/LetsEncrypt.tsx
+++ src/badges/LetsEncrypt.tsx
@@ -3,6 +3,6 @@ import React from "react"
 import { Badge } from "../badge"
 import type { BadgeProps } from "../badge"
 
-const LetsEncrypt = (props: BadgeProps) => <Badge name="Let’s Encrypt" {...props} backgroundColor="#003A70" />
+const LetsEncrypt = (props: BadgeProps) => <Badge name="Let's Encrypt" {...props} backgroundColor="#003A70" />
 
 export default LetsEncrypt
--- src/badges/Macys.tsx
+++ src/badges/Macys.tsx
@@ -3,6 +3,6 @@ import React from "react"
 import { Badge } from "../badge"
 import type { BadgeProps } from "../badge"
 
-const Macys = (props: BadgeProps) => <Badge name="Macy’s" {...props} backgroundColor="#E21A2C" />
+const Macys = (props: BadgeProps) => <Badge name="Macy's" {...props} backgroundColor="#E21A2C" />
 
 export default Macys
