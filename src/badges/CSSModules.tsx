ccced63d47227cc0030ccbb93a0a02a816bd98c7
Auto Fetch <nick@wylynko.com>
2022-04-25 00:58:25 +0000

    Updated json

---
+++ src/badges/CSSModules.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const CSSModules = (props: BadgeProps) => <Badge name="CSS Modules" {...props} backgroundColor="#000000" />
+
+export default CSSModules
---
+++ src/badges/SpaCy.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const SpaCy = (props: BadgeProps) => <Badge name="spaCy" {...props} backgroundColor="#09A3D5" />
+
+export default SpaCy
