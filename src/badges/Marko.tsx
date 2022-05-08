a6db4ae25a21e9c84d1a72b4e0b54c6c2852afc5
Auto Fetch <nick@wylynko.com>
2022-05-08 01:02:38 +0000

    Updated json

---
+++ src/badges/Marko.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Marko = (props: BadgeProps) => <Badge name="Marko" {...props} backgroundColor="#2596BE" />
+
+export default Marko
---
+++ src/badges/Remix.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Remix = (props: BadgeProps) => <Badge name="Remix" {...props} backgroundColor="#000000" />
+
+export default Remix
