67624e2fac2ac585959b2eb3660966ad4b351f4f
Auto Fetch <nick@wylynko.com>
2022-05-09 01:05:04 +0000

    Updated json

---
+++ src/badges/GTK.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const GTK = (props: BadgeProps) => <Badge name="GTK" {...props} backgroundColor="#7FE719" />
+
+export default GTK
---
+++ src/badges/RuboCop.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const RuboCop = (props: BadgeProps) => <Badge name="RuboCop" {...props} backgroundColor="#000000" />
+
+export default RuboCop
