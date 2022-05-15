c0359cfaeaee305425e21f962ed77b6ac74e0192
Auto Fetch <nick@wylynko.com>
2022-05-15 01:17:37 +0000

    Updated json

---
+++ src/badges/EnterpriseDB.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const EnterpriseDB = (props: BadgeProps) => <Badge name="EnterpriseDB" {...props} backgroundColor="#FF3E00" />
+
+export default EnterpriseDB
---
+++ src/badges/SteamDeck.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const SteamDeck = (props: BadgeProps) => <Badge name="Steam Deck" {...props} backgroundColor="#1A9FFF" />
+
+export default SteamDeck
