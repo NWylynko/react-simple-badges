78988bd876aff2ac64115136df361403470d78e8
Auto Fetch <nick@wylynko.com>
2022-05-17 01:09:02 +0000

    Updated json

---
+++ src/badges/Lapce.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Lapce = (props: BadgeProps) => <Badge name="Lapce" {...props} backgroundColor="#3B82F6" />
+
+export default Lapce
---
+++ src/badges/Wise.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Wise = (props: BadgeProps) => <Badge name="Wise" {...props} backgroundColor="#00B9FF" />
+
+export default Wise
