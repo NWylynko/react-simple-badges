a325daa11f54c65c451d5512f57224f9f0cd8641
Auto Fetch <nick@wylynko.com>
2022-06-16 01:08:48 +0000

    Updated json

---
+++ src/badges/Fauna.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Fauna = (props: BadgeProps) => <Badge name="Fauna" {...props} backgroundColor="#3A1AB6" />
+
+export default Fauna
---
+++ src/badges/HiBob.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const HiBob = (props: BadgeProps) => <Badge name="Hi Bob" {...props} backgroundColor="#E42C51" />
+
+export default HiBob
