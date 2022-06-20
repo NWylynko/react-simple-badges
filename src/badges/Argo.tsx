66c538038c85554fd4cc8cdb04b735d38ed68294
Auto Fetch <nick@wylynko.com>
2022-06-20 01:11:20 +0000

    Updated json

---
+++ src/badges/Argo.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Argo = (props: BadgeProps) => <Badge name="Argo" {...props} backgroundColor="#EF7B4D" />
+
+export default Argo
---
+++ src/badges/Flatpak.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Flatpak = (props: BadgeProps) => <Badge name="Flatpak" {...props} backgroundColor="#4A90D9" />
+
+export default Flatpak
