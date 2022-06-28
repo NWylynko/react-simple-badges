fa203589a6bfea10817df6f516217603d069f6a3
Auto Fetch <nick@wylynko.com>
2022-06-28 01:14:43 +0000

    Updated json

---
+++ src/badges/BorgBackup.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const BorgBackup = (props: BadgeProps) => <Badge name="BorgBackup" {...props} backgroundColor="#00DD00" />
+
+export default BorgBackup
---
+++ src/badges/UnoCSS.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const UnoCSS = (props: BadgeProps) => <Badge name="UnoCSS" {...props} backgroundColor="#333333" />
+
+export default UnoCSS
