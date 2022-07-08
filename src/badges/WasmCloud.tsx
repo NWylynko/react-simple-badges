972e300994e5843ef07e2a8f2fd95efcaca2984f
Auto Fetch <nick@wylynko.com>
2022-07-08 01:18:07 +0000

    Updated json

---
+++ src/badges/WasmCloud.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const WasmCloud = (props: BadgeProps) => <Badge name="wasmCloud" {...props} backgroundColor="#00BC8E" />
+
+export default WasmCloud
---
+++ src/badges/ZebraTechnologies.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const ZebraTechnologies = (props: BadgeProps) => <Badge name="Zebra Technologies" {...props} backgroundColor="#000000" />
+
+export default ZebraTechnologies
