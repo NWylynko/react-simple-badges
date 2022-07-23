28b18a36e8348ab0168de29e5207a02d817ef019
Auto Fetch <nick@wylynko.com>
2022-07-23 01:13:19 +0000

    Updated json

---
+++ src/badges/Electronbuilder.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Electronbuilder = (props: BadgeProps) => <Badge name="electron-builder" {...props} backgroundColor="#FFFFFF" />
+
+export default Electronbuilder
---
+++ src/badges/FOne.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const FOne = (props: BadgeProps) => <Badge name="F1" {...props} backgroundColor="#E10600" />
+
+export default FOne
