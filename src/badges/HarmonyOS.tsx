a244c3538d4ba6f9df02d1e2e30d36bd1aa8238a
Auto Fetch <nick@wylynko.com>
2022-08-26 01:19:56 +0000

    Updated json

---
+++ src/badges/HarmonyOS.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const HarmonyOS = (props: BadgeProps) => <Badge name="HarmonyOS" {...props} backgroundColor="#000000" />
+
+export default HarmonyOS
---
+++ src/badges/TableCheck.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const TableCheck = (props: BadgeProps) => <Badge name="TableCheck" {...props} backgroundColor="#7935D2" />
+
+export default TableCheck
