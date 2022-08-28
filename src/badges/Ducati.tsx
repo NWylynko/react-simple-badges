da1209a9a3b247191122e35b818d5b684e698698
Auto Fetch <nick@wylynko.com>
2022-08-28 01:19:44 +0000

    Updated json

---
+++ src/badges/Ducati.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Ducati = (props: BadgeProps) => <Badge name="Ducati" {...props} backgroundColor="#CC0000" />
+
+export default Ducati
---
+++ src/badges/LegacyGames.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const LegacyGames = (props: BadgeProps) => <Badge name="Legacy Games" {...props} backgroundColor="#144B9E" />
+
+export default LegacyGames
