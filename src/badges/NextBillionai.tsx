b1bbc107baf9acef0858d386187cabc76d79f392
Auto Fetch <nick@wylynko.com>
2022-09-04 01:18:08 +0000

    Updated json

---
+++ src/badges/NextBillionai.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const NextBillionai = (props: BadgeProps) => <Badge name="NextBillion.ai" {...props} backgroundColor="#8D5A9E" />
+
+export default NextBillionai
---
+++ src/badges/OpenProject.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const OpenProject = (props: BadgeProps) => <Badge name="OpenProject" {...props} backgroundColor="#0770B8" />
+
+export default OpenProject
