985ebbb577cbc6ddee57b890245d55551e92c7ba
Auto Fetch <nick@wylynko.com>
2022-09-06 01:42:16 +0000

    Updated json

---
+++ src/badges/SecurityScorecard.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const SecurityScorecard = (props: BadgeProps) => <Badge name="SecurityScorecard" {...props} backgroundColor="#7033FD" />
+
+export default SecurityScorecard
---
+++ src/badges/XO.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const XO = (props: BadgeProps) => <Badge name="XO" {...props} backgroundColor="#5ED9C7" />
+
+export default XO
