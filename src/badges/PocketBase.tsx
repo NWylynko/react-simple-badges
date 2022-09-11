8bedb69a3d55ec85058b57e7adaddaa5f8c61aa6
Auto Fetch <nick@wylynko.com>
2022-09-11 01:26:10 +0000

    Updated json

---
+++ src/badges/PocketBase.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const PocketBase = (props: BadgeProps) => <Badge name="PocketBase" {...props} backgroundColor="#B8DBE4" />
+
+export default PocketBase
---
+++ src/badges/UptimeKuma.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const UptimeKuma = (props: BadgeProps) => <Badge name="Uptime Kuma" {...props} backgroundColor="#5CDD8B" />
+
+export default UptimeKuma
