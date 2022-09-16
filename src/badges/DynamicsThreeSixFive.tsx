67f32bf6f93503dafc0e9ded3de3862a8833a001
Auto Fetch <nick@wylynko.com>
2022-09-16 01:28:45 +0000

    Updated json

--- src/badges/DynamicsThreeSixFive.tsx
+++ src/badges/DynamicsThreeSixFive.tsx
@@ -3,6 +3,6 @@ import React from "react"
 import { Badge } from "../badge"
 import type { BadgeProps } from "../badge"
 
-const DynamicsThreeSixFive = (props: BadgeProps) => <Badge name="Dynamics 365" {...props} backgroundColor="#002050" />
+const DynamicsThreeSixFive = (props: BadgeProps) => <Badge name="Dynamics 365" {...props} backgroundColor="#0B53CE" />
 
 export default DynamicsThreeSixFive
