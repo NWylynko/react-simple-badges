a7cf8aeaceed06d9f494d5b8d8f8a6d1da107a95
Auto Fetch <nick@wylynko.com>
2022-09-17 01:22:25 +0000

    Updated json

---
+++ src/badges/GoogleHome.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const GoogleHome = (props: BadgeProps) => <Badge name="Google Home" {...props} backgroundColor="#4285F4" />
+
+export default GoogleHome
