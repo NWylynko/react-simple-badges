675c71dd7f682f9dd8f852e97e55c957fadf2766
Auto Fetch <nick@wylynko.com>
2022-10-02 01:41:49 +0000

    Updated json

---
+++ src/badges/Honey.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Honey = (props: BadgeProps) => <Badge name="Honey" {...props} backgroundColor="#FF6801" />
+
+export default Honey
---
+++ src/badges/Prevention.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Prevention = (props: BadgeProps) => <Badge name="Prevention" {...props} backgroundColor="#44C1C5" />
+
+export default Prevention
