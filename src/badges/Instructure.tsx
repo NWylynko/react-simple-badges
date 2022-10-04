74263b103a264e38ab39609842b3bbb658cfa24b
Auto Fetch <nick@wylynko.com>
2022-10-04 01:28:07 +0000

    Updated json

---
+++ src/badges/Instructure.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Instructure = (props: BadgeProps) => <Badge name="Instructure" {...props} backgroundColor="#2A7BA0" />
+
+export default Instructure
---
+++ src/badges/RSocket.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const RSocket = (props: BadgeProps) => <Badge name="RSocket" {...props} backgroundColor="#EF0092" />
+
+export default RSocket
