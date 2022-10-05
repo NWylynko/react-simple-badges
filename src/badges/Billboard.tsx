59f6c15995d6534ea7d5ec60ebd21392b06a4fa5
Auto Fetch <nick@wylynko.com>
2022-10-05 01:26:09 +0000

    Updated json

---
+++ src/badges/Billboard.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Billboard = (props: BadgeProps) => <Badge name="Billboard" {...props} backgroundColor="#000000" />
+
+export default Billboard
