51b1d92b3c61f5069e58e097b4e9bd795f077f61
Auto Fetch <nick@wylynko.com>
2022-10-07 01:40:47 +0000

    Updated json

---
+++ src/badges/Backblaze.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge"
+import type { BadgeProps } from "../badge"
+
+const Backblaze = (props: BadgeProps) => <Badge name="Backblaze" {...props} backgroundColor="#E21E29" />
+
+export default Backblaze
