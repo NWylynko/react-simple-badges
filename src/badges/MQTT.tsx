ccca997a1ff3499db8bef21335e2882c2b435494
Auto Fetch <nick@wylynko.com>
2022-10-31 01:28:14 +0000

    Updated json

---
+++ src/badges/MQTT.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge.js"
+import type { BadgeProps } from "../badge.js"
+
+const MQTT = (props: BadgeProps) => <Badge name="MQTT" backgroundColor="#660066" {...props} />
+
+export default MQTT
---
+++ src/badges/Ravelry.tsx
@@ -0,0 +1,8 @@
+
+import React from "react"
+import { Badge } from "../badge.js"
+import type { BadgeProps } from "../badge.js"
+
+const Ravelry = (props: BadgeProps) => <Badge name="Ravelry" backgroundColor="#EE6E62" {...props} />
+
+export default Ravelry
